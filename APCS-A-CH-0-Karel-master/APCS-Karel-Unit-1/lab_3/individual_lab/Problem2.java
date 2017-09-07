2daae1ce88c5d0169e263e5888c124b08f79100b
cshapoorian <cshaporian@gmail.com>
2017-09-07 12:40:48 -0700

    Lab 3

--- APCS-A-CH-0-Karel-master/APCS-Karel-Unit-1/lab_3/individual_lab/Problem2.java
+++ APCS-A-CH-0-Karel-master/APCS-Karel-Unit-1/lab_3/individual_lab/Problem2.java
@@ -13,9 +13,111 @@ public class Problem2 extends Robot
     {
         super(st, av, dir, beep);
     }
-
+    
     public void carpetRooms(){
-        
+        carpet();
+        carpet();
+        carpet();
+        carpet();
+        carpet();
+        carpet();
+        carpet();
+        carpet();
     }
+    
+    public void carpet() {
+        enterRoom();
+        checkBox();
+        exitRoom();
+    }
+    
+    public void exitRoom() {
+        if(!facingSouth()) {
+            turnLeft();
+            exitRoom();
+        }
+        if(facingSouth()) {
+            move();
+            turnLeft();
+        }
+    }
+    
+    public void turnRight() {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+    
+    public void enterRoom() {
+        move();
+        turnLeft();
+        move();
+        turnRight();
+    }
+    
+    public boolean checkRight() {
+        if(checkLeft()==true) {
+           if(!facingEast()) {
+               turnLeft();
+               checkLeft();
+           }
+           if(facingEast()) {
+               if(!frontIsClear()) {
+                return true;
+               }
+               else {
+                return false;
+               }
+           }
+           return false;
+        }
+        return false;
+    }
+    
+    public boolean checkTop() {
+        if(checkLeft()==true) {
+           if(!facingNorth()) {
+               turnLeft();
+               checkLeft();
+           }
+           if(facingNorth()) {
+               if(!frontIsClear()) {
+                return true;
+               }
+               else {
+                return false;
+               }
+           }
+           return false;
+        }
+        return false;
+    }
+    
+    public boolean checkLeft() {
+        if(!facingWest()) {
+            turnLeft();
+            checkLeft();
+        }
+        if(facingWest()) {
+            if(!frontIsClear()) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        return false;
+    }
+    
+    public void checkBox() {
+        if (checkRight()==true) {
+            if (checkTop()==true) {
+                 if(checkLeft()==true) {
+                     putBeeper();
+                 }
+            }
+        }
+    }
+    
 }
 
--- APCS-A-CH-0-Karel-master/APCS-Karel-Unit-1/lab_3/individual_lab/Problem2Driver.java
+++ APCS-A-CH-0-Karel-master/APCS-Karel-Unit-1/lab_3/individual_lab/Problem2Driver.java
@@ -18,7 +18,7 @@ public class Problem2Driver implements Directions
         World.setBeeperColor(Color.magenta);
         World.setStreetColor(Color.blue);
         World.setNeutroniumColor(Color.green.darker());
-        World.setDelay(10);  
+        World.setDelay(30);  
         World.setVisible(true);
     }
 }
